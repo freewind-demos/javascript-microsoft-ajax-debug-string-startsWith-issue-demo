01db34d3c64094ac9c6ab25f5e7ec53858c1ed9c
peng.li <nowindlee@gmail.com>
2019-08-17 14:49:29 +0800

    demo

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
-Js Hello World Demo
-===================
+JavaScript Microsoft Ajax Debug JS "String.startsWith" Issue Demo
+=================================================================
 
-Open `index.html` in your browser.
+MS的`MicrosoftAjax.debug.js`定义了如下的startsWith:
 
-You will see "Hello, JS!" on the page.
+```
+String.prototype.startsWith = function String$startsWith(prefix) {
+    var e = Function._validateParams(arguments, [
+        {name: "prefix", type: String}
+    ]);
+    if (e) throw e;
+    return (this.substr(0, prefix.length) === prefix);
+}
+```
+
+当传的参数个数大于1时，它会直接报错，而其它库（比如core-js/babel-polyfill）会向其传入两个参数，导致报错。
+
+这可以认为是`MicrosoftAjax.debug.js`的一个bug，在它没有被解决前，我们只能通过判断并替换掉它的内部实现。
+
+```
+npm install
+npm demo
+```
+
+Open browser console to see logging.
--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-var main = document.getElementById("main");
-main.innerHTML = "Hello, JS!";
+// will throw
+// 'Sys.ParameterCountException: Sys.ParameterCountException: Parameter count mismatch.'
+// due to 'MicrosoftAjax.debug.js' polyfill
+function testStartsWith() {
+  console.log('> testStartsWith');
+  console.log('testStartsWith:', 'hello'.startsWith('ell', 1));
+}
+
+function defineStartsWith() {
+  console.log('> defineStartsWith');
+  String.prototype.startsWith = function (search, index = 0) {
+    return (this.substr(index, search.length) === search);
+  };
+}
+
+function checkAndFix() {
+  console.log('> checkAndFix');
+  try {
+    testStartsWith();
+  } catch (error) {
+    console.log('error: ', error.message);
+    if (error.message === 'Sys.ParameterCountException: Parameter count mismatch.') {
+      defineStartsWith();
+    }
+  }
+}
+
+checkAndFix();
+
+testStartsWith();
 
